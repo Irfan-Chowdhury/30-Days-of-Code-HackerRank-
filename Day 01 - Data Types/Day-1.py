8c9ea5df71823901cdd31af5730c7f0645bdf751
Irfan-Chowdhury <irfanchowdhury80@gmail.com>
2020-04-25 04:30:25 +0600

    Day-1

---
+++ Day 01 - Data Types/Day-1.py
@@ -0,0 +1,20 @@
+# Declare second integer, double, and String variables.
+i2 = int(input())
+d2 = float(input())
+s2 = str(input())
+
+# Read and save an integer, double, and String to your variables.
+sum_i = int(i+i2)
+sum_d = float(d+d2)
+concate_s = str(s+s2) 
+
+# Print the sum of both integer variables on a new line.
+print(sum_i)
+
+# Print the sum of the double variables on a new line.
+print(round(sum_d,1))
+
+# Concatenate and print the String variables on a new line
+print(concate_s)
+
+# The 's' variable above should be printed first.
